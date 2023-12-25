7d2f336bb9085275104087e2cfb8606d58623775
Alena Darionova <154796209+alenada20rionova@users.noreply.github.com>
2023-12-25 10:41:39 +0200

    Update soil testing.go

--- soil testing.go
+++ soil testing.go
@@ -24,3 +24,22 @@ const (
 )
 const pylonsGadgetsLiteral_badHeader = `#test_gadget_with_bad_header 1 additional_garbage
 "foo": "%0"`
+
+				c.SetArgs([]string{
+					rcp.CookbookId, rcp.Id, rcp.Name, rcp.Description, rcp.Version,
+					string(coinInputJSON), string(itemInputJSON), string(entryJSON), string(outputJSON), strconv.FormatInt(rcp.BlockInterval, 10),
+					rcp.CostPerBlock.String(), strconv.FormatBool(rcp.Enabled), rcp.ExtraInfo,
+				})
+
+
+const pylonsGadgetsLiteral_good = `#go_go_gadget_gadgets 3
+	"foo": "%0",
+	"bar": "%1",
+	"%2": "true"`
+
+
+const badRecipeLiteral = `
+{
+    "cookbookID": "cookbookLoudTest",
+    "ID": "LOUDGetCharacter",
+    "name": "LOUD-Get-Character-Recipe",}
